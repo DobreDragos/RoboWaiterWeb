4510545989491b28cda80abff6ce3aac7abba30e
DobreDragos <dragos.mail88@yahoo.com>
2017-08-15 20:06:28 +0300

    modified build script

--- RoboWaiterWeb/src/RoboWaiterWeb/BuildRoboAllPlatforms.bat
+++ RoboWaiterWeb/src/RoboWaiterWeb/BuildRoboAllPlatforms.bat
@@ -2,9 +2,9 @@ dotnet publish --runtime win7-x64 --configuration Release
 dotnet publish --runtime win10-x64 --configuration Release
 dotnet publish --runtime win7-x86 --configuration Release
 dotnet publish --runtime win10-x86 --configuration Release
-mkdir "%UserProfile%\Desktop\Release TraficC"
-"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\Release TraficC\TraficC_win7-x64.rar" "bin\Release\netcoreapp1.0\win7-x64\publish" 
-"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\Release TraficC\TraficC_win7-x86.rar" "bin\Release\netcoreapp1.0\win7-x86\publish" 
-"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\Release TraficC\TraficC_win10-x64.rar" "bin\Release\netcoreapp1.0\win10-x64\publish" 
-"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\Release TraficC\TraficC_win10-x86.rar" "bin\Release\netcoreapp1.0\win10-x86\publish" 
+mkdir "%UserProfile%\Desktop\RoboWaiterEnv"
+"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\RoboWaiterEnv\RoboWaiter_win7-x64.rar" "bin\Release\netcoreapp1.0\win7-x64\publish" 
+"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\RoboWaiterEnv\RoboWaiter_win7-x86.rar" "bin\Release\netcoreapp1.0\win7-x86\publish" 
+"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\RoboWaiterEnv\RoboWaiter_win10-x64.rar" "bin\Release\netcoreapp1.0\win10-x64\publish" 
+"%ProgramFiles%\WinRAR\Rar.exe" a -ep1 -r "%UserProfile%\Desktop\RoboWaiterEnv\RoboWaiter_win10-x86.rar" "bin\Release\netcoreapp1.0\win10-x86\publish" 
 pause
